549a983a698b9cc9e04a9063378711b7ba298f58
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-03 08:54:18 -0600

    Initial Commit

---
+++ Brain WormTests/Brain_WormTests.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_WormTests.swift
+//  Brain WormTests
+//
+//  Created by Gwen Thelin on 10/3/24.
+//
+
+import Testing
+@testable import Brain_Worm
+
+struct Brain_WormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
