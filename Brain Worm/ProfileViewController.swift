979bfa6dd059dbe6c14ac7f7820ce0b3d1e31f01
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-04 08:39:19 -0600

    Moved the makeStyle functions into a new function called styalizeText() in order to make the viewDidLoad to look cleaner

--- Brain Worm/ProfileViewController.swift
+++ Brain Worm/ProfileViewController.swift
@@ -49,14 +49,18 @@ class ProfileViewController: UIViewController {
 		setStyle(style: textStyle, label: label, letterSpacing: letterSpacing)
 	}
 	
-	override func viewDidLoad() {
-        super.viewDidLoad()
-
+	func styalizeText() {
 		makeStyle(label: bioLabel, alignment: .center, lineHeight: 1.3, letterSpacing: 0.8)
 		makeStyle(label: nameLabel, alignment: .center, lineHeight: 1.1, letterSpacing: -1.2)
 		makeStyle(label: usernameLabel, alignment: .center, lineHeight: 1.2, letterSpacing: -0.45)
 		makeStyle(label: postLabel, alignment: .left, lineHeight: 1.5, letterSpacing: 0.8)
 		makeStyle(label: timestampLabel, alignment: .left, lineHeight: 1.1, letterSpacing: 1)
+	}
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+		styalizeText()
     }
     
 
