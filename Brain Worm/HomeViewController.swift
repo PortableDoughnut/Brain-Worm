a324e8e51273ecf3f2c1af27d1be23a8b4a313f6
PortableDoughnut <gwen.thelin@gmail.com>
2024-10-07 09:41:48 -0600

    Made too many changes to remember

--- Brain Worm/HomeViewController.swift
+++ Brain Worm/HomeViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+	@IBOutlet weak var astarionTimestampLabel: UILabel!
+	@IBOutlet weak var astarionPostLabel: UILabel!
+	@IBOutlet weak var astarionUsernameLabel: UILabel!
+	@IBOutlet weak var astarionNameLabel: UILabel!
+	
+	let setFont: TypographyController = .init()
+	
+	func setHeaderTextStyle(label: UILabel) {
+		setFont.makeStyle(label: label, alignment: .left, lineHeight: 1.3, letterSpacing: -0.28)
+	}
+	
+	func setPostStyle(label: UILabel) {
+		setFont.makeStyle(label: label, alignment: .left, lineHeight: 1.386, letterSpacing: 0.026)
+	}
+	
+	func setTimestampStyle(label: UILabel) {
+		
+	}
+	
+	func setTextStyle() {
+		setHeaderTextStyle(label: astarionNameLabel)
+		setHeaderTextStyle(label: astarionUsernameLabel)
+		setPostStyle(label: astarionPostLabel)
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		setTextStyle()
 	}
 
 
